4819d88f7b3460960c8ed2735474299f1bc6172d
P2539038 <P2539038@my365.dmu.ac.uk>
2022-01-19 23:31:27 +0000

    Created test methods and classes using TDD

---
+++ Co-op System/Co-opClassLibraryy/clsAdvisor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Co_opClassLibraryy
+{
+    public class clsAdvisor
+    {
+        public int AdvisorID { get; set; }
+
+        public bool Find(int advisorID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
